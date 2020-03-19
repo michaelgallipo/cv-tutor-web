58bd7d3aa5a7a32cdaf243bc6970a064e5ca9589
michael <michael.gallipo@gmail.com>
2020-03-19 16:08:20 -0400

    Added edit and delete methods for tutors

--- app/controllers/api/tutors_controller.rb
+++ app/controllers/api/tutors_controller.rb
@@ -37,4 +37,37 @@ skip_before_action :verify_authenticity_token
       end
   end
 
+  def update
+    @tutor = Tutor.find(params[:id])
+      @tutor.name = params[:name] || @tutor.name
+      @tutor.street = params[:street] || @tutor.street
+      @tutor.city = params[:city] || @tutor.city
+      @tutor.state = params[:state] || @tutor.state
+      @tutor.zip = params[:zip] || @tutor.zip
+      @tutor.school = params[:school] || @tutor.school
+      @tutor.email = params[:email] || @tutor.email
+      @tutor.phone = params[:phone] || @tutor.phone
+      @tutor.phone_visible = params[:phone_visible] || @tutor.phone_visible
+      @tutor.about = params[:about] || @tutor.about
+      @tutor.grade_min = params[:grade_min] || @tutor.grade_min
+      @tutor.grade_max = params[:grade_max] || @tutor.grade_max
+      @tutor.rate = params[:rate] || @tutor.rate
+      @tutor.accept_new = params[:accept_new] || @tutor.accept_new
+
+      if @tutor.save
+        render "show.json.jbuilder"
+      else
+        render json: {errors: @tutor.errors.full_message}, status: :unprocessable_entity
+      end
+
+  end
+
+  def destroy
+    @tutor = Tutor.find(params[:id])
+    if @tutor.delete
+      render json: {message: "Account deleted"}
+    else
+      render json: {errors: @tutor.errors.full_message}, status: :unprocessable_entity
+  end
+
 end
