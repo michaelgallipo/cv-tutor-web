f988d316961995202b50cb19346e0bc54dd86186
michael <michael.gallipo@gmail.com>
2020-04-16 12:54:02 -0400

    Fixed issue with boolean fields on edit

--- app/controllers/api/tutors_controller.rb
+++ app/controllers/api/tutors_controller.rb
@@ -58,22 +58,22 @@ skip_before_action :verify_authenticity_token
       @tutor.school = params[:school] || @tutor.school
       @tutor.email = params[:email] || @tutor.email
       @tutor.phone = params[:phone] || @tutor.phone
-      @tutor.phone_visible = params[:phone_visible] || @tutor.phone_visible
+      @tutor.phone_visible = params[:phone_visible] 
       @tutor.about = params[:about] || @tutor.about
-      @tutor.math = params[:math] || @tutor.math
-      @tutor.reading = params[:reading] || @tutor.reading
-      @tutor.science = params[:science] || @tutor.science
-      @tutor.social_studies = params[:social_studies] || @tutor.social_studies
-      @tutor.special_needs = params[:special_needs] || @tutor.special_needs
-      @tutor.spelling = params[:spelling] || @tutor.spelling
+      @tutor.math = params[:math] 
+      @tutor.reading = params[:reading] 
+      @tutor.science = params[:science] 
+      @tutor.social_studies = params[:social_studies]
+      @tutor.special_needs = params[:special_needs] 
+      @tutor.spelling = params[:spelling] 
       @tutor.grade_min = params[:grade_min] || @tutor.grade_min
       @tutor.grade_max = params[:grade_max] || @tutor.grade_max
       @tutor.rate = params[:rate] || @tutor.rate
-      @tutor.accept_new = params[:accept_new] || @tutor.accept_new
+      @tutor.accept_new = params[:accept_new]
 
       if params[:password] != ""
-        @tutor.password = params[:password] || @tutor.password
-        @tutor.password_confirmation = params[:password_confirmation] || @tutor.password_confirmation
+        @tutor.password = params[:password] 
+        @tutor.password_confirmation = params[:password_confirmation] 
       end
     
       if @tutor.save
